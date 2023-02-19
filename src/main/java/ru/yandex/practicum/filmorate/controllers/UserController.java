b0077b201105fa6b57946b5640a5db219b13d9c3
schitov <schitovladimir@yandex.ru>
2023-02-19 05:29:06 +0300

    bagfixing: add new wxceptions

--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exceptions.ValidException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import java.time.LocalDate;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -20,6 +21,8 @@ public class UserController {
     public User addUser(@Valid @RequestBody User user) {
         log.info("add user {}", user.toString());
         checkAndFillUserName(user);
+        checkSpaces(user);
+        checkBirthday(user);
         user.setId(generatorId());
         users.put(user.getId(), user);
         return user;
@@ -36,6 +39,8 @@ public class UserController {
         log.info("update user {}", user);
         checkUserInUsers(user);
         checkAndFillUserName(user);
+        checkSpaces(user);
+        checkBirthday(user);
         users.replace(user.getId(), user);
         return user;
     }
@@ -49,6 +54,18 @@ public class UserController {
     private void checkUserInUsers(User user) {
         if (!users.containsKey(user.getId())) {
             throw new ValidException("User with number is not existed");
+        }
+    }
+
+    private void checkSpaces(User user) {
+        if (user.getLogin().contains(" ")) {
+            throw new ValidException("Login musn't contain spaces");
+        }
+    }
+
+    private void checkBirthday(User user) {
+        if(!user.getBirthday().isBefore(LocalDate.now())) {
+            throw new ValidException("This birthday is not existed");
         }
     }
 
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,9 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
@@ -17,7 +14,7 @@ import java.time.LocalDate;
 public class User extends Item {
     @Email
     private String email;
-    @NonNull
+    @NotNull
     @NotBlank
     @NotEmpty
     private String login;
